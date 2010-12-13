1bd486631982cdec592767ba2969bb70cefe3d25
Abner Silva <abner@collabora.co.uk>
2010-12-13 17:23:06 -0300

    First implementation of the TimeRange type.
    The TimeRange class stores a pair of int64 that represents a start and
    an end in the time.

---
+++ include/QtZeitgeist/DataModel/timerange.h
@@ -0,0 +1,127 @@
+/*
+ * This file is part of QtZeitgeist.
+ *
+ * Copyright (C) 2010 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef QTZEITGEIST_TIMERANGE_H_
+#define QTZEITGEIST_TIMERANGE_H_
+
+#include <QDateTime>
+#include <QMetaType>
+#include <QDBusArgument>
+
+namespace QtZeitgeist
+{
+
+namespace DataModel
+{
+
+class TimeRangePrivate;
+
+/**
+ * @class TimeRange event.h event.h
+ * @brief The representation of an interval in time, marked by a
+ * beginning and an end.
+ *
+ * The start and the end point are represented in number of milliseconds
+ * since the Unix Epoch.
+
+ * @author Abner Silva <abner@collabora.co.uk>
+ */
+class Q_DECL_EXPORT TimeRange : public QObject
+{
+    Q_OBJECT
+    Q_PROPERTY(qint64 begin READ begin WRITE setBegin)
+    Q_PROPERTY(qint64 end READ end WRITE setEnd)
+
+public:
+
+    /**
+     * Default constructor.
+     */
+    explicit TimeRange(QObject *parent = 0);
+
+    /**
+     * Copy constructor.
+     */
+    TimeRange(const TimeRange &source, QObject *parent = 0);
+
+    /**
+     * Data initializing constructor.
+     *
+     * @param startTime the start point of the time range.
+     * @param endTime the end point of the time range.
+     */
+    TimeRange(qint64 startTime, qint64 endTime);
+
+    /**
+     * Destructor.
+     */
+    virtual ~TimeRange();
+
+    /**
+     * Returns the start point of the time range.
+     *
+     * @returns the begin of the time range.
+     */
+    qint64 begin() const;
+
+    /**
+     * Set the start value/point of the time range.
+     *
+     * @param begin a value for the start point in milliseconds.
+     */
+    void setBegin(qint64 begin);
+
+    /**
+     * Returns the end point of the time range.
+     *
+     * @returns the end of the time range.
+     */
+    qint64 end() const;
+
+    /**
+     * Set the end value/point of the time range.
+     *
+     * @param end a value for the start point in milliseconds.
+     */
+    void setEnd(qint64 end);
+
+    TimeRange & operator = (const TimeRange &source);
+
+private:
+
+    // D Pointer.
+    class TimeRangePrivate * const d;
+
+    // Marshalers operators.
+    friend QDBusArgument & operator << (QDBusArgument &argument,
+            const TimeRange &event);
+    friend const QDBusArgument & operator >> (const QDBusArgument &argument,
+            TimeRange &event);
+};
+
+};
+
+};
+
+// Declare types as Qt Meta Type.
+Q_DECLARE_METATYPE(QtZeitgeist::DataModel::TimeRange)
+
+#endif // QTZEITGEIST_TIMERANGE_H_
---
+++ src/datamodel/timerange.cpp
@@ -0,0 +1,129 @@
+/*
+ * This file is part of QtZeitgeist.
+ *
+ * Copyright (C) 2010 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#include "QtZeitgeist/DataModel/TimeRange"
+
+namespace QtZeitgeist
+{
+
+namespace DataModel
+{
+
+class TimeRangePrivate
+{
+public :
+    TimeRangePrivate(qint64 _begin = 0, qint64 _end = 0)
+        : begin(_begin),
+        end(_end)
+    {
+    }
+
+    qint64 begin;
+    qint64 end;
+};
+
+TimeRange::TimeRange(QObject *parent)
+    : d(new TimeRangePrivate())
+{
+    Q_ASSERT(d);
+}
+
+TimeRange::TimeRange(const TimeRange & source, QObject *parent)
+    : d(new TimeRangePrivate())
+{
+    Q_ASSERT(d);
+
+    // Copy the source attribute's value.
+    d->begin = source.d->begin;
+    d->end = source.d->end;
+}
+
+TimeRange::TimeRange(qint64 begin, qint64 end)
+    : d(new TimeRangePrivate(begin, end))
+{
+}
+
+TimeRange::~TimeRange()
+{
+    delete d;
+}
+
+qint64 TimeRange::begin() const
+{
+    return d->begin;
+}
+
+void TimeRange::setBegin(qint64 begin)
+{
+    d->begin = begin;
+}
+
+qint64 TimeRange::end() const
+{
+    return d->end;
+}
+
+
+void TimeRange::setEnd(qint64 end)
+{
+    d->end = end;
+}
+
+TimeRange &TimeRange::operator = (const TimeRange &source)
+{
+    // Copy the source attribute's value.
+    if (this != &source) {
+        d->begin = source.d->begin;
+        d->end = source.d->end;
+    }
+
+    return *this;
+}
+
+QDBusArgument & operator << (QDBusArgument &argument,
+        const TimeRange &timeRange)
+{
+    argument.beginStructure();
+
+    argument
+        << timeRange.d->begin
+        << timeRange.d->end;
+
+    return argument;
+}
+
+const QDBusArgument & operator >> (const QDBusArgument &argument,
+        TimeRange &timeRange)
+{
+    argument.beginStructure();
+
+    argument
+        >> timeRange.d->begin
+        >> timeRange.d->end;
+
+    argument.endStructure();
+
+    return argument;
+}
+
+};
+
+};
